70f63da6704abb741e430cce816deddb60d39878
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-27 15:01:12 +0530

    updated nav elements

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar(){
         {name:"Careers"}
     ]
     return(
-        <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container 
+        <nav className="flex w-full items-center justify-between px-[5px] py-[14px] lg:container 
         lg:mx-auto lg:px-20">
             <div className="flex items-center">
             <Image
@@ -19,16 +19,17 @@ export function Navbar(){
             width={100}
             height={50}
             />
-             <div className="hidden lg:flex pl-[74px] gap-x-[56px]">
+             <div className="hidden lg:flex pl-[74px] gap-x-[56px] px-[20px] py-[16px]">
                {nanvLinks.map((item,index)=>
                 (
                     <p className="text-[##36485c] font-medium" key={index}>{item.name}</p>
                 ))}
                </div>
             </div>
-           <div className="flex gap-x-5">
-            <p className="hidden lg:block font-medium text-[#36485c] pr-[56px] pt-6">Open Account</p>
-            <div className="flex items-center gap-x-2">
+            
+            <div className="flex items-center gap-x-[56px]">
+            <span className="hidden lg:block font-medium text-[#36485c]">Open Account</span>
+            <span className="hidden lg:block font-medium text-[#36485c] ">Sign in</span>
                 <Image
                  src={User}
                 alt="user"
@@ -36,7 +37,7 @@ export function Navbar(){
                 height={30}
                 />
               
-                <span className="hidden font-medium text-[#36485c] lg:block">Sign in</span>
+                
             </div>
             <div>
                 <Image
@@ -47,7 +48,7 @@ export function Navbar(){
                 className="lg:hidden"
                 />
             </div>
-           </div>
+           
 
         </nav>
     )
